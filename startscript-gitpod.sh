443307fcb51f6b90265eaba28e5d7e26c562f66c
reverb821 <125830200+reverb821@users.noreply.github.com>
2023-02-19 21:18:42 +0100

    Initialize gitpod-config-improvements

---
+++ startscript-gitpod.sh
@@ -0,0 +1,12 @@
+# git config
+git config pull.rebase false
+
+# sqlite config
+echo -e ".headers on\n.mode column" > ~/.sqliterc
+
+
+echo installeren van benodigde node.js packages
+echo dit kan even duren
+
+yarn install
+
